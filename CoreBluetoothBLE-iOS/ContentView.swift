6d45b75c15db462201e333adffca6cc296d2eb64
asadjaved <>
2024-05-31 18:34:11 +0500

    *Handled the App State Restoration

--- CoreBluetoothBLE-iOS/ContentView.swift
+++ CoreBluetoothBLE-iOS/ContentView.swift
@@ -10,21 +10,34 @@ import SwiftUI
 struct ContentView: View {
     var bluetoothManager = BLEPeripheralManager()
     @State private var currentDate = Date.now
+    @State private var name: String = ""
+    func nameString(name: String) -> String {
+        if name.trimmingCharacters(in: .whitespaces).count != 0 {
+            return name
+        }
+        return "Defalut Text"
+    }
     let timer = Timer.publish(every: 60, on: .main, in: .common).autoconnect()
     var body: some View {
-        VStack {
-            Text("Peripherals Device")
-            Button {
-                bluetoothManager.sendMessage("Hello!!!@#")
-            } label: {
-                Text("Send")
+        NavigationView {
+            VStack {
+                TextField("Enter your message", text: $name)
+                    .padding()
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                Button {
+                    bluetoothManager.sendMessage(nameString(name: name))
+                    name = ""
+                } label: {
+                    Text("Send")
+                }
+
+
             }
-
-
+            .navigationTitle("Peripheral Device")
+            .padding()
+            .onReceive(timer) { _ in
+                bluetoothManager.sendMessage("Hello!!!@#")
         }
-        .padding()
-        .onReceive(timer) { _ in
-            bluetoothManager.sendMessage("Hello!!!@#")
         }
     }
 }
